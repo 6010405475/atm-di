632268ffd9c47893cb2a2aee529dd6634560e8ce
Max <siripong.chu@ku.th>
2019-09-22 22:59:24 +0700

    update

--- src/main/java/atm_annotation_database/Reader.java
+++
@@ -1,8 +0,0 @@
-package atm_annotation_database;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface Reader {
-    public Map<Integer, Customer> readCustomers() throws IOException;
-}
